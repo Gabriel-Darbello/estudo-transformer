3873aaffcc7fdad9c21de783dd1c10fb9f5649e3
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-07-04 19:13:23 -0300

    feat: sistema que pega input do usuário e converte em embeddings armazenando elas em um arquivo JSON

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.embedding.embedding import Embedding
+from src.tokenizer.tokenizer import Tokenizer
+
+embedding = Embedding()
+tokenizer = Tokenizer()
+
+user_msg = str(input("Digite algo: "))
+tokens = tokenizer.tokenize(user_msg)
+embedding.generate_embedding(tokens)
+
