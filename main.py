78f51fbf4aea80d50c34bdb17d6eedb5b622c703
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-07-06 09:18:48 -0300

    feat: sequencia completa unindo tokens, embeddings e atenttion

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from src.embedding.embedding import Embedding
 from src.tokenizer.tokenizer import Tokenizer
+from src.self_attention.self_attention import SelfAttention
 
 embedding = Embedding()
 tokenizer = Tokenizer()
+self_attention = SelfAttention()
 
 user_msg = str(input("Digite algo: "))
 tokens = tokenizer.tokenize(user_msg)
+print(f"tokens: \n {tokens}")
+
 embeddings = embedding.generate_embedding(tokens)
-print(embeddings)
+print(f"embeddings: \n {embeddings}")
+
+attention = self_attention.forward(embeddings)
+print(f"attention: \n {attention}")
 
